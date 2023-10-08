8512924e22cccd5001e9fed1abaced8be00cd826
zildjibian <84656572+zildjibian@users.noreply.github.com>
2023-10-08 21:41:26 +0800

    Update DrawSpaceAPI.lua

--- compiled/DrawSpaceAPI.lua
+++ compiled/DrawSpaceAPI.lua
@@ -94,7 +94,7 @@
 	DrawSpace:Queue(a: Action)
 	DrawSpace:FireQueue()
 
-	DrawSpace:ClearAll()
+	DrawSpace:ClearAll() -- shortcut to DrawSpace:CreateAction('clearall'):Fire()
 
 
 	Drawspace.Preview.PartCount = 0
@@ -126,7 +126,9 @@ end
 
 -- example script (draws a floating dot in the middle of the room)
 
-local pos = Vector3.new(20.68, 40, 318.69)
+-- DrawSpace:ClearAll()
+
+local pos = Vector3.new(20.68, 30, 318.69)
 local normals = {
 	Vector3.new(1, 0, 0),
 	Vector3.new(-1, 0, 0),
@@ -140,7 +142,7 @@ for _, normal in pairs(normals) do
 	DrawSpace:CreateAction('Draw.Dot', {
 		Position = pos,
 		Normal = normal,
-		Size = 1
+		Size = 10
 	}):Queue()
 end
 
