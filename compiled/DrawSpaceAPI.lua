2243301664cc66eeffc1d569d2d29a68a1cdd64d
zildjibian <84656572+zildjibian@users.noreply.github.com>
2023-10-08 22:14:42 +0800

    Update DrawSpaceAPI.lua

--- compiled/DrawSpaceAPI.lua
+++ compiled/DrawSpaceAPI.lua
@@ -124,26 +124,4 @@ if not DrawSpace then
 	loadstring(game:GetService('HttpService'):JSONDecode(game:HttpGet("https://github.com/zildjibian/scripts/raw/main/inits.json")).DrawSpaceAPI)()
 end
 
--- example script (draws a floating dot in the middle of the room)
-
--- DrawSpace:ClearAll()
-
-local pos = Vector3.new(20.68, 30, 318.69)
-local normals = {
-	Vector3.new(1, 0, 0),
-	Vector3.new(-1, 0, 0),
-	Vector3.new(0, 1, 0),
-	Vector3.new(0, -1, 0),
-	Vector3.new(0, 0, 1),
-	Vector3.new(0, 0, -1),
-}
-
-for _, normal in pairs(normals) do
-	DrawSpace:CreateAction('Draw.Dot', {
-		Position = pos,
-		Normal = normal,
-		Size = 10
-	}):Queue()
-end
-
-DrawSpace:FireQueue()
+-- example script: https://github.com/zildjibian/scripts/raw/main/others/dsa.ex.lua
