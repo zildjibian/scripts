0f00f3c402e05a1a2ecc6e49a4dffda7a6254019
zildjibian <84656572+zildjibian@users.noreply.github.com>
2024-01-23 19:58:12 +0800

    Create client.lua

---
+++ mobile/websocket/client.lua
@@ -0,0 +1,12 @@
+local wso = WebSocket.connect("ws://YOUR_IPV4:8765")
+
+-- .OnMessage  : RBXScriptSignal
+-- :Send       : (content: string) -> ()
+
+local function print(...)
+	local t = {...}
+	for i, v in t do t[i] = tostring(v) end
+	wso:Send(table.concat(t, ", "))
+end
+
+print("A", "B", "C")
