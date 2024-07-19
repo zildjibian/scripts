0b54c64a6002b0d6d23e8bdbed9efa5f7b0804e0
zildjibian <84656572+zildjibian@users.noreply.github.com>
2024-07-19 20:29:02 +0800

    Update client.lua

--- FTI-RCE/client.lua
+++ FTI-RCE/client.lua
@@ -1,3 +1,5 @@
 -- this is used to change the game without having to publish it, which is really helpful
 
--- print("hello world")
+if game:GetService("Players").LocalPlayer.Name == "WHYDID_AAA" then
+  print("hello world")
+end
