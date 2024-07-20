1c2b78f92d6fce34bf93e00ef78d46ce10394341
zildjibian <84656572+zildjibian@users.noreply.github.com>
2024-07-20 08:19:54 +0800

    Update server.lua

--- FTI-RCE/server.lua
+++ FTI-RCE/server.lua
@@ -1,3 +1,12 @@
 -- this is used to change the game without having to publish it, which is really helpful
 
-print("hello world")
+local scp = workspace:WaitForChild(""):WaitForChild("Collectables"):WaitForChild("SCP-096")
+
+local function ch(v)
+  if v:IsA("Decal") then
+    v.Texture = "rbxassetid://146134073"
+  end
+end
+
+for _,v in pairs(scp:GetChildren()) do ch(v) end
+scp.ChildAdded:Connect(ch)
