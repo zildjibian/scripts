07af33b7dbcb4fe9252b899ff454a6ce172ddc31
zildjibian <84656572+zildjibian@users.noreply.github.com>
2024-07-20 08:37:04 +0800

    Update server.lua

--- FTI-RCE/server.lua
+++ FTI-RCE/server.lua
@@ -1,13 +1,22 @@
 -- server runs in Luau
 -- client runs in vLua
 
-local scp = workspace:WaitForChild("Collectables"):WaitForChild("SCP-096")
+local colls = workspace:WaitForChild("Collectables")
 
-local function ch(v)
-  if v:IsA("Decal") then
-    v.Texture = "rbxassetid://146134073"
-  end
+local function changeTexture(name, target, desc)
+  spawn(function()
+  	local coll = colls:WaitForChild(name)
+  
+  	local function ch(v) if v:IsA("Decal") then v.Texture = target end end
+  
+  	for _,v in coll:GetChildren() do ch(v) end
+  	coll.ChildAdded:Connect(ch)
+  	
+  	if desc then
+  		require(coll:WaitForChild("Config")).Description = desc
+  	end
+  end)
 end
 
-for _,v in scp:GetChildren() do ch(v) end
-scp.ChildAdded:Connect(ch)
+changeTexture("SCP-096", "rbxassetid://146134073", "boo\n\ndecal uploaded by @joelwari")
+changeTexture("Meow Skulls", "rbxassetid://12243376450", "fortnite\n\ndecal uploaded by @Solar_Elimz2") -- i never knew the og was... uhhh...
